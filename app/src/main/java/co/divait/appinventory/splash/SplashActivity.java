815be157c28cddd2b091363c58ea961f7544af64
dp17 <diegoparra@tiendosqui.com>
2017-01-08 22:59:10 -0500

    Get data from the server

--- app/src/main/java/co/divait/appinventory/splash/SplashActivity.java
+++ app/src/main/java/co/divait/appinventory/splash/SplashActivity.java
@@ -2,14 +2,38 @@ package co.divait.appinventory.splash;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import co.divait.appinventory.R;
+import co.divait.appinventory.utils.Parser;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements Parser.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
+        Parser.getData(this, this);
+    }
+
+    @Override
+    public void onApps(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onTitle(String title) {
+        Toast.makeText(this, title, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRights(String rights) {
+        Toast.makeText(this, rights, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onError(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/co/divait/appinventory/utils/Parser.java
+++ app/src/main/java/co/divait/appinventory/utils/Parser.java
@@ -1,8 +1,56 @@
 package co.divait.appinventory.utils;
 
+import android.content.Context;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import co.divait.appinventory.network.HttpCall;
+
 /**
  * Created by divait on 8/01/2017.
+ *
+ * Parse the JSON Data to objects.
  */
 
 public class Parser {
+
+    public interface Callback {
+        void onApps (String data);
+        void onTitle (String title);
+        void onRights (String rights);
+        void onError (String error);
+    }
+
+    public static void getData(Context context, final Callback callback) {
+        HttpCall httpCall = new HttpCall(context);
+        httpCall.getMainData(new HttpCall.Callback() {
+
+            @Override
+            public void onSuccess(String response) {
+                parseMainData(response, callback);
+            }
+
+            @Override
+            public void onError(String error) {
+                callback.onError(error);
+            }
+        });
+    }
+
+    private static void parseMainData(String response, Callback callback) {
+        try {
+            JSONObject json = new JSONObject(response);
+            JSONObject feed = json.getJSONObject("feed");
+
+            callback.onTitle(feed.getJSONObject("title").getString("label"));
+            callback.onRights(feed.getJSONObject("rights").getString("label"));
+
+            JSONArray apps = feed.getJSONArray("entry");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            callback.onError("Incorrect Data.");
+        }
+    }
 }
